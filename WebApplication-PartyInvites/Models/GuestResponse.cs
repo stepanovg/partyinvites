c390be2e15513c0efbe621f05574cc77f23ce425
Stepanov Georgiy <stepanov.g@hotmail.com>
2016-05-25 00:30:20 +0300

    home pc

--- WebApplication-PartyInvites/Models/GuestResponse.cs
+++ WebApplication-PartyInvites/Models/GuestResponse.cs
@@ -12,7 +12,7 @@ namespace PartyInvites.Models
         public string Email { get; set; }
         public string Phone { get; set; }
         public bool? WillAttend { get; set; }
-        //hdfgh
+        //hdfgh1
     }
 }
 
